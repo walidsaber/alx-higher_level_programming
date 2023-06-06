e378e644110ba303239f4f7d2c05e2d0aefc4024
walidsaber <walid_saber123@outlook.com>
2023-06-06 05:59:23 -0700

    upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        print("{}\n".format(chr(ord(str[i])-32)), end='')
+        print("{}".format(chr(ord(str[i])-32)), end='')
+    print("\n")
