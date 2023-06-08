7e13fd6ebe76bbfaeb7249ce2c07bd276c1c361e
walidsaber <walid_saber123@outlook.com>
2023-06-08 13:01:03 +0100

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+def args_counts():
+    args = sys.argv[1:]
+    argc = len(args)
+    if argc > 0:
+        dot = ":"
+    else:
+        dot = ""
+    print(f"{argc} arguments{dot}")
+    for i in range(len(args)):
+        print("i: {args[i]}")
+
+args_counts()
