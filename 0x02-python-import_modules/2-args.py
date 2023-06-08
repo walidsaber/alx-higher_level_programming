d82be59ce13f97fdc39a8835cbbd236be4a1b700
walidsaber <walid_saber123@outlook.com>
2023-06-08 05:12:44 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,11 +7,13 @@ if __name__ == "__main__":
     def args_counts():
         args = sys.argv[1:]
         argc = len(args)
-        if argc > 0:
+        if argc > 1:
+            dot = "s:"
+        elif argc == 1:
             dot = ":"
         else:
-            dot = "."
-        print(f"{argc} arguments{dot}")
+            dot = "s."
+        print(f"{argc} argument{dot}")
         for i in range(len(args)):
             print(f"{i+1}: {args[i]}")
 
