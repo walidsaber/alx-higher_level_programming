036290b8720fa74b643e22ef772f3fabbfb73f2a
walidsaber <walid_saber123@outlook.com>
2023-06-08 13:04:32 +0100

    add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-def args_counts():
-    args = sys.argv[1:]
-    argc = len(args)
-    if argc > 0:
-        dot = ":"
-    else:
-        dot = ""
-    print(f"{argc} arguments{dot}")
-    for i in range(len(args)):
-        print("i: {args[i]}")
+    def args_counts():
+        args = sys.argv[1:]
+        argc = len(args)
+        if argc > 0:
+            dot = ":"
+        else:
+            dot = ""
+        print(f"{argc} arguments{dot}")
+        for i in range(len(args)):
+            print("i: {args[i]}")
 
 args_counts()
