257c361c65aa8dc6c10ed97ee0ed1669cfcc777e
walidsaber <walid_saber123@outlook.com>
2023-06-09 14:29:13 -0700

    deleted at index

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if not my_list:
+        return None
+    elif idx < 0 and idx >= len(my_list):
+        return my_list
+    del my_list[idx]
+    return my_list
