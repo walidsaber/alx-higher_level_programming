7805b308868615147b811a6a2008d7633e6f905d
walidsaber <walid_saber123@outlook.com>
2023-06-09 22:10:22 +0100

    divisible

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 
-
+def divisible_by_2(my_list=[]):
+    xlist = []
+    x = 0
+    for i in my_list:
+        if i % 2 == 0:
+            xlist += [True]
+        else:
+            xlist += [False]
+        x += 1
+    return xlist
