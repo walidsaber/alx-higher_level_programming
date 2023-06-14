3c04d49e4af170d7e075f1776444b8407772f932
walidsaber <walid_saber123@outlook.com>
2023-06-14 05:35:02 -0700

    matrxi

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,10 +2,5 @@
 
 def square_matrix_simple(matrix=[]):
     xmatrix = []
-    row = []
-    for x in matrix:
-        row = []
-        for i in x:
-            row.append(i**2)
-        xmatrix.append(row)
+    xmatrix = [[i**2 for i in sec] for sec in matrix]
     return xmatrix
