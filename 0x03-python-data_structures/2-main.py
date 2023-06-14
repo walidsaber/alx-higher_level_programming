ac7e0a099ab5c3037bd11c0223144905f07e8c09
walidsaber <walid_saber123@outlook.com>
2023-06-14 05:38:03 -0700

    d

--- 0x03-python-data_structures/2-main.py
+++ 0x03-python-data_structures/2-main.py
@@ -3,8 +3,12 @@ replace_in_list = __import__('2-replace_in_list').replace_in_list
 
 my_list = [1, 2, 3, 4, 5]
 idx = 3
+xlist = [1, 2, 3, 4, 6]
+blist = []
 new_element = 9
 new_list = replace_in_list(my_list, idx, new_element)
-
+xdlist = replace_in_list(xlist, 5, 7)
+xxdlist = replace_in_list(blist, 2, 9)
+print(xdlist)
 print(new_list)
 print(my_list)
