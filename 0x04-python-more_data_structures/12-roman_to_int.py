c36dbdc35623540af7e8f26f07e0990e6a93522a
walidsaber <walid_saber123@outlook.com>
2023-06-14 10:08:43 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
-        return None
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
     roman_dic = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
     result = 0
     x = 0
