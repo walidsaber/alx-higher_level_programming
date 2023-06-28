c02a88b12e22928b27fa2c5db332cc96a43ac2c7
walidsaber <walid_saber123@outlook.com>
2023-06-27 18:45:53 -0700

    t2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square."""
+
+
+class Square:
+    """square """
+
+    def __init__(self, size=0):
+        """new Square.
+        Args:
+            size (int): int
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
