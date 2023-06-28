04d6bb0f26e23d3482273dee5ec49bec5faf78cb
walidsaber <walid_saber123@outlook.com>
2023-06-27 18:41:47 -0700

    t2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square."""
+
+
+class Square:
+    """square."""
+
+    def __init__(self, size):
+        """new Square.
+
+        Args:
+            size (int): int
+        """
+        self.__size = size
