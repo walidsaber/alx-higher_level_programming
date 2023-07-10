30452c6520b78bb1b947549614c253f02121d2b4
walidsaber <walid_saber123@outlook.com>
2023-07-10 05:26:44 -0700

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""SUBCLASS"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """SQUARE"""
+
+    def __init__(self, size):
+        """Initializing"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
