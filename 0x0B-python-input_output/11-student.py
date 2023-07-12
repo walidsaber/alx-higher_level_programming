94033cf40065d1483aa52f4d742778ac2628dfd6
walidsaber <walid_saber123@outlook.com>
2023-07-11 18:14:39 -0700

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""pascal"""
+
+
+def pascal_triangle(n):
+    """pascal tr"""
+    rows = [[1 for j in range(i + 1)] for i in range(n)]
+    for n in range(n):
+        for i in range(n - 1):
+            rows[n][i + 1] = sum(rows[n - 1][i:i + 2])
+    return rows
